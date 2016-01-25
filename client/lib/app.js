cbe553fcd6260e2439cb480aa52b80c770fe4df0
Justin Ugerio <justinugerio@users.noreply.github.com>
2016-01-25 00:18:34 -0800

    Data binding with add and remove buttons
    Signed-off-by: Justin Ugerio <justinugerio@users.noreply.github.com>

--- client/lib/app.js
+++ client/lib/app.js
@@ -9,12 +9,25 @@ angular.module('socially').directive('partiesList', function () {
         controllerAs: 'partiesList',
         controller: function ($scope, $reactive) {
             $reactive(this).attach($scope);
-    
+            
+            this.yourmother = 'your mother eats poo poo!';
+                
+            this.newParty = { };
+            
             this.helpers({
                 parties: function () {
                     return Parties.find({});
                 }
             });
+            
+            this.addParty = function () {
+                Parties.insert(this.newParty);
+                this.newParty = { };
+            };
+            
+            this.removeParty = function (party) {
+                Parties.remove({_id: party._id});  
+            };
         }
         
     };
