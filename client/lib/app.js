97b42bb04b973b62682847ee64553adcd9c1e5ce
justin.ugerio <justinugerio@users.noreply.github.com>
2016-02-03 17:00:26 -0800

    Drag and drop tests
    Signed-off-by: justin.ugerio <justinugerio@users.noreply.github.com>

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,35 +1,4 @@
 
-angular.module('socially', ['angular-meteor']);
+angular.module('socially', ['angular-meteor', angularDragula(angular)]);
 
-angular.module('socially').directive('partiesList', function () {
-    
-    return {
-        restrict: 'E',
-        templateUrl: 'client/parties-list.html',
-        controllerAs: 'partiesList',
-        controller: function ($scope, $reactive) {
-            $reactive(this).attach($scope);
-            
-            this.yourmother = 'your mother eats poo poo!';
-                
-            this.newParty = { };
-            
-            this.helpers({
-                parties: function () {
-                    return Parties.find({});
-                }
-            });
-            
-            this.addParty = function () {
-                Parties.insert(this.newParty);
-                this.newParty = { };
-            };
-            
-            this.removeParty = function (party) {
-                Parties.remove({_id: party._id});  
-            };
-        }
-        
-    };
-        
-});
+
